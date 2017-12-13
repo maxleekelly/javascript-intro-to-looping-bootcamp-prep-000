fc4db8562c72a08ddab85cd7ca1d0d2272bd057d
Max Kelly <maxleekelly@gmail.com>
2017-12-13 13:57:58 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,10 @@
 function forLoop(array) {
-  for (i = 1, i < 26, i++ ) {
-    return i;
+  for (i = 1; i < 26; i++) {
+    if (i === 1) {
+      array.push("I am 1 strange loop.");
+    } else {
+      array.push(`I am ${i} strange loops.`);
+    }
   }
+  return array;
 }
